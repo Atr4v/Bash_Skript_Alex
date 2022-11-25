130955428a37dd541cdf78b05d08d3f6286a9e9d
Alexander Goebbels <go.alexander@web.de>
2022-11-25 13:43:28 +0100

    Initial commit

---
+++ count_lines.sh
@@ -0,0 +1,8 @@
+# count_lines.sh
+# 2022 - Goebbels
+# Counts the lines of all TSV files
+# in the current folder.
+
+echo "Count all lines of TSV files"
+wc -l *tsv
+echo "Counting done - have a nice day"
